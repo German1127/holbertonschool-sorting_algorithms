e5a7e77ce9fdd07d18d0be637835aca99ee04943
German1127 <kleverk2023@gmail.com>
2023-09-07 05:43:21 -0700

    update sort.h

--- sort.h
+++ sort.h
@@ -1,8 +1,8 @@
-#ifndef _SORT_
-#define _SORT_
+#ifndef SORTING_ALGORITHMS_H
+#define SORTING_ALGORITHMS_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -11,18 +11,21 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
- 
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/* Prototypes */
+/* helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* prototypes of the project */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif
+#endif /* SORTING_ALGORITHMS_H */
